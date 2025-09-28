33e4b7f22bf19086110ae74da1bffd67ec006b35
nicolcortes-angel <nicolmurciacortes7@gmail.com>
2025-09-28 13:54:02 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'inicio', component: HomeComponent, title: 'Inicio'},
+    {path:'admin', component: AdminComponent, title: 'Dashboard'},
+    {path:'login', component: LoginComponent, title: 'Inicio Sesion'},
+    {path:'not-found', component: NotFoundComponent, title: '404'},
+    {path:'products', component: ProductsComponent, title: 'Productos'},
+    {path:'registro', component: RegisterComponent, title: 'Registro'},
+];
