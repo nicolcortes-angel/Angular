6f2e2708b56b9ad9f6b369fc522be7196765b583
nicolcortes-angel <nicolmurciacortes7@gmail.com>
2025-10-05 00:22:49 -0500

    juevesclase1sem

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,10 +7,11 @@ import { ProductsComponent } from './pages/products/products.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
-    {path:'inicio', component: HomeComponent, title: 'Inicio'},
+    {path:'', component: HomeComponent, title: 'Inicio'},
     {path:'admin', component: AdminComponent, title: 'Dashboard'},
     {path:'login', component: LoginComponent, title: 'Inicio Sesion'},
-    {path:'not-found', component: NotFoundComponent, title: '404'},
     {path:'products', component: ProductsComponent, title: 'Productos'},
     {path:'registro', component: RegisterComponent, title: 'Registro'},
+    {path:'**', component: NotFoundComponent, title: '404'}, //siempre debe ir al final 
 ];
+
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
